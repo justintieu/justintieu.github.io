069ec35e9276c3b9fc03810808dfdc4021dacc86
Justin Tieu <tieujustin@gmail.com>
2015-06-28 16:24:47 -0700

    cleaned up formatting

--- split/js/script.js
+++ split/js/script.js
@@ -1,18 +1,18 @@
 var modify_buttons = function(id) {
-	$(id).click(function() {
-		var c = "." + id.substr(1,3) + "-btn";
-		$(c).toggleClass("disabled");
-	});
+    $(id).click(function() {
+        var c = "." + id.substr(1, 3) + "-btn";
+        $(c).toggleClass("disabled");
+    });
 
 }
 
 var main = function() {
-	modify_buttons('#tax_box');
-	modify_buttons('#tip_box');
-	$('#tax_value').attr({
-		'data-toggle': "modal",
-	 	'data-target': "#taxModal" 
-	});
+    modify_buttons('#tax_box');
+    modify_buttons('#tip_box');
+    $('#tax_value').attr({
+        'data-toggle': "modal",
+        'data-target': "#taxModal"
+    });
 }
 
 $(document).ready(main);
