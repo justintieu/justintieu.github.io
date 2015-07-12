3f83fed78e423952d8f35f6a6b32ea517fee3c27
Justin Tieu <tieujustin@gmail.com>
2015-07-12 14:41:43 -0700

    settings module + split bill function

--- split/js/controllers/MobileController.js
+++ split/js/controllers/MobileController.js
@@ -1,28 +1,71 @@
 app.controller('MobileController', ['$scope', function($scope) {
-    $scope.persons = {};
-    $scope.num_persons = 0;
+    $scope.persons = [];
     $scope.bill = 0;
     $scope.tax = 9;
     $scope.tip = 15;
+    $scope.round = false;
+    $scope.total_bill = 0;
+    $scope.num_splitters = 0;
     $scope.add_new_person = function() {
-        $('#newPersonModal').modal('hide');
         var name = $('#name').val();
-        var splitting = $.fn.bootstrapSwitch.defaults.state;
-        var amount_owed = (splitting) ? $('#amount_owed').val() : null;
-        $scope.persons[name] = new person(name, amount_owed, splitting);
-        $scope.num_persons++;
-        console.log($scope.persons);
-        $scope.update_bill();
-        $('#new_person_form')[0].reset();
+        var splitting = $('input[name="splitting"]').bootstrapSwitch('state');
+        var amount_owed = (splitting) ? null : $('#amount_owed').val() - 0;
+        if((name.length > 0 && splitting) || (!splitting && $('#amount_owed').val().length > 0)) {
+            $scope.persons.push(new person(name, amount_owed, splitting));
+            $('#newPersonModal').modal('hide');
+            if(splitting) {
+                $scope.num_splitters++;
+            }
+            $scope.update_bill();
+            reset_new_person_form();
+        }
     }
 
     $scope.remove_person = function(name) {
-        $scope.persons.remove(name);
-        $scope.num_persons--;
+        for (var i = 0; i < $scope.persons.length; i++) {
+            if ($scope.persons[i].name == name) {
+                if($scope.persons[i].splitting) {
+                    $scope.num_splitters--;
+                }
+                $scope.persons.splice(i, 1);
+                break;
+            }
+        }
         $scope.update_bill();
     }
 
-    $scope.update_bill = function() {
+    $scope.update_settings = function() {
+        var tax_input = $('#tax_percentage').val() - 0;
+        var tip_input = $('#tip_percentage').val() - 0;
+        if(tax_input >= 0 && tax_input <= 100 && tip_input >= 0 && tip_input <= 100) {
+            $scope.tax = tax_input;
+            $scope.tip = tip_input;
+            $scope.rounding = $('input[name="rounding"]').bootstrapSwitch('state');
+            $('#settingsModal').modal('hide');
+            $scope.update_bill();
+        }
+    }
 
+    $scope.update_bill = function() {
+        $scope.total_bill = $scope.bill + ($scope.tax/100*$scope.bill) + ($scope.tip/100*$scope.bill);
+        //consider nonsplitters first
+        for(var i = 0; i < $scope.persons.length; i++) {
+            if(!$scope.persons[i].splitting) {
+                var curr_amount = $scope.persons[i].amount_owed + ($scope.tax/100*$scope.persons[i].amount_owed) + ($scope.tip/100*$scope.persons[i].amount_owed);
+                $scope.persons[i].final_amount = curr_amount;
+                $scope.total_bill = $scope.total_bill - curr_amount;
+            }
+        }
+        //consider everyone after
+        for(var i = 0; i < $scope.persons.length; i++) {
+            if($scope.persons[i].splitting) {
+                $scope.persons[i].final_amount = ($scope.total_bill >= 0) ? $scope.total_bill/$scope.num_splitters : 0;
+            }
+        }
+    }
+
+    $scope.set_bill = function() {
+        $scope.bill = $('#bill_amount_input').val() - 0;
+        $scope.update_bill();
     }
 }]);
--- split/js/mobile.js
+++ split/js/mobile.js
@@ -9,14 +9,29 @@ $('input[name="splitting"]').on('switchChange.bootstrapSwitch', function(event, 
 	}
 });
 
+$('#newPersonModal').on('hide.bs.modal', function (e) {
+  reset_new_person_form();
+})
+
+var reset_new_person_form = function() {
+    $('#name').val("");
+    if(!$('input[name="splitting"]').bootstrapSwitch('state')) {
+        $('input[name="splitting"]').bootstrapSwitch('state', true, true);
+    }
+    $('#amount_owed').val("");
+    $('#amount_owed').prop("disabled", true);
+}
+
 function person(full_name, amount_owed, splitting) {
     this.full_name = full_name;
     this.amount_owed = amount_owed;
     this.splitting = splitting;
+    this.final_amount = 0;
 }
 
 var main = function() {
-	$("[name='splitting']").bootstrapSwitch();
+	$("input[name='splitting']").bootstrapSwitch();
+	$("input[name='rounding']").bootstrapSwitch();
 }
 
 $(document).ready(main);
